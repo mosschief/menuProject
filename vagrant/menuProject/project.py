dc3b09a8b711786efc7e8a72d0dba7db54d7471a
Sean Moss <Sean Moss>
2015-11-27 18:16:47 -0600

    changed ip

--- vagrant/menuProject/project.py
+++ vagrant/menuProject/project.py
@@ -344,4 +344,4 @@ def deleteMenuItem(restaurant_id, menu_id):
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.1.18', port=5000)
